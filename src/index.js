9d251fd4ea8e471dddfbd1f9b15e46b8d19a831f
Leonardo Bühring Müller <leonardo.muller@fockink.ind.br>
2020-10-28 13:15:36 -0300

    commit from Fockink 1028

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+/*
+Middleware:
+
+Interceptador de requisições que interromper totalmente a requisição ou alterar dados da requisição
+
+*/
+
+
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+//Middleware
+function logRequest(request, response, next) {
+  const { method, url} = request
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel)
+
+  return next() //Próximo Middleware
+}
+
+function validateProjectId(request, response, next){
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid Project ID."});
+  }
+  return next();
+}
+
+app.use(logRequest);
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
