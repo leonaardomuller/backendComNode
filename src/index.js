10784ced9ee8c45cc9bc904cc225416fa1f56120
Leonardo Muller <leonaardo.muller@gmail.com>
2020-10-27 21:00:44 -0300

    nada

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: "Hello Leonardo" })
+  return response.json({ message: "Hello World 2" })
 });
  
 app.listen(3333, () => {
